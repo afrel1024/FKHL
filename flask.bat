3a21e4a5f0ad3f6a07f186d8fc7ea15db2ad1f86
DisHailiangEDUTechnologyDepartment <135300649+afrel1024@users.noreply.github.com>
2026-05-23 06:08:45 +0800

    Delete flask.bat

--- flask.bat
+++
@@ -1,4 +0,0 @@
-pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple flask
-pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple pyinstaller
-pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple pyopenssl
-pause
